4c801a0979308b8a9b7447b6c7c842926cb9560d
Maciej Leśniak <maciek@sparkmedia.pl>
2018-08-31 16:18:25 +0200

    initial commit

---
+++ src/main/java/pl/sparkidea/security/securedapp/SecuredappApplication.java
@@ -0,0 +1,12 @@
+package pl.sparkidea.security.securedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuredappApplication.class, args);
+    }
+}
